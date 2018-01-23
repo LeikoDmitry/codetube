6a00c9dc00f8f2a78e4131c5bd386c415f9c203c
Dmitry <d.leyko@itransition.com>
2018-01-23 16:41:15 +0300

    Add login functions

--- app/urls.py
+++ app/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('login', action.Auth.login_action, name='login'),
     path('register', action.Auth.register_action, name='register'),
     path('reset', action.Auth.reset_action, name='reset'),
-    path('email', action.Auth.email_action, name='email')
+    path('email', action.Auth.email_action, name='email'),
+    path('logout', action.Auth.logout_action, name='logout')
 ]
--- app/views/action.py
+++ app/views/action.py
@@ -1,20 +1,49 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
+from django.shortcuts import redirect, get_object_or_404
+from django.contrib.auth.decorators import login_required
+from app.forms.form import UserRegistrationForm
+
 
 
 class Auth:
     """Auth classes action"""
 
     @staticmethod
+    @login_required(login_url='/login')
     def index_action(request):
         return render(request, 'app/index.html')
 
     @staticmethod
     def login_action(request):
-        return render(request, 'app/login.html')
+        if request.method != 'POST':
+            return render(request, 'app/login.html')
+        else:
+            email = request.POST['email']
+            password = request.POST['password']
+            try:
+                user = User.objects.get(email=email)
+                user_login = authenticate(request, username=user.username, password=password)
+                if user_login is not None:
+                    login(request, user_login)
+                    return redirect('tube:index')
+                else:
+                    return redirect('tube:login')
+            except User.DoesNotExist:
+                return redirect('tube:login')
+
 
     @staticmethod
     def register_action(request):
-        return render(request, 'app/register.html')
+        if request.method != 'POST':
+            return render(request, 'app/register.html')
+        else:
+            form = UserRegistrationForm(request.POST)
+            if not form.is_valid():
+                return redirect('tube:register')
+            else:
+                return redirect('tube:login')
 
     @staticmethod
     def reset_action(request):
@@ -23,6 +52,11 @@ class Auth:
     @staticmethod
     def email_action(request):
         return render(request, 'app/email.html')
+
+    @staticmethod
+    def logout_action(request):
+        logout(request)
+        return redirect('tube:login')
 
 
 class Channel:
